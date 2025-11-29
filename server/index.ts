dec4066b40210a3c9b88cfca7eb043303b62decd
FutureBlock <malkiel@futureblock.in>
2025-11-29 18:29:32 +0530

    change port number on index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,6 @@
-import "dotenv/config";
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
-
-
 
 const app = express();
 app.use(express.json());
@@ -63,11 +60,10 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-   const port = parseInt(process.env.PORT || "5000", 10);
-
-app.listen(port, "127.0.0.1", () => {
-  console.log(`Server running on http://localhost:${port}`);
+  const port = parseInt(process.env.PORT || '5000', 10);
+  server.listen(port, "0.0.0.0", () => {
+  log(`serving on port ${port}`);
 });
-
-
 })();
+
+
